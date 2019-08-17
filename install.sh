307210192284849fe46f4ccc9a1e46bb8dc59841
Jesse Wattenbarger <jesse.j.wattenbarger@gmail.com>
2019-08-17 12:12:33 -0400

    Include installing racket in install.sh.

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+set -e
+wget https://mirror.racket-lang.org/installers/7.4/racket-7.4-x86_64-linux.sh
+chmod +x racket-7.4-x86_64-linux.sh
+./racket-7.4-x86_64-linux.sh
+rm ./racket-7.4-x86_64-linux.sh
+export PATH=$HOME/racket/bin:$PATH
 raco pkg install minikanren
 raco pkg install pie
 
